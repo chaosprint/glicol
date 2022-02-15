d79b7c57149a31bf8702c93080c78b2f65193d2f
chaosprint <chaosprint@gmail.com>
2022-02-15 21:09:04 +0100

    fix version number

--- js/src/glicol.js
+++ js/src/glicol.js
@@ -1,6 +1,6 @@
 // when publish, change the exact version number
 // in local testing, comment the version out!
-// window.version = "v0.7.2"
+window.version = "v0.7.2"
 const source = window.version ? `https://cdn.jsdelivr.net/gh/chaosprint/glicol@${version}/js/src/` : "src/"
 
 window.loadDocs = async () => {
