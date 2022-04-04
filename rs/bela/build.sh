3daac620934d245ca8d3789544f490d161f2b864
chaosprint <chaosprint@gmail.com>
2022-04-04 11:02:46 +0200

    optimise bela compiling

--- rs/bela/build.sh
+++ rs/bela/build.sh
@@ -1,6 +1,7 @@
+set -e
 NAME="glicol"
 
 export PATH=$PATH:`pwd`/arm-bela-linux-gnueabihf/bin
-cargo build --target=armv7-unknown-linux-gnueabihf --example $NAME
+RUSTFLAGS="-C target-cpu=cortex-a8" cargo build --target=armv7-unknown-linux-gnueabihf --example $NAME
 scp target/armv7-unknown-linux-gnueabihf/debug/examples/$NAME root@bela.local:~
 ssh root@bela.local
