0f39ec6be3efbab57ffd326049c347a41966564b
chaosprint <chaosprint@gmail.com>
2022-04-05 23:58:34 +0200

    no need to use fetch for eval

--- js/src/glicol.js
+++ js/src/glicol.js
@@ -1,7 +1,7 @@
 // when publish, change the exact version number
 // in local testing, comment the version out!
 
-window.version = "v0.11.1"
+window.version = "v0.11.2"
 
 window.source = window.version ? `https://cdn.jsdelivr.net/gh/chaosprint/glicol@${version}/js/src/` : "src/"
 fetch(source+`utils.js`).then(res=>res.text()).then( text => // ${window.version ? ".min": ""}
@@ -337,9 +337,31 @@ window.loadModule = async () => {
               alert(`Error: ${info}`)
             } else {
               
-              await fetch(source+`error.js`).then(res=>res.text()).then( text => // ${window.version ? ".min": ""}
-                eval(text)
-              )
+              // await fetch(source+`error.js`).then(res=>res.text()).then( text => // ${window.version ? ".min": ""}
+                eval(`const posRegex = /(?<=pos\\[)[^\\]]+?(?=\\])/g // /(?<=pos\\[])[^\\]]*(?=\\])/g 
+                const lineRegex = /(?<=line\\[)[^\\]]+?(?=\\])/g
+                const colRegex = /(?<=col\\[)[^\\]]+?(?=\\])/g
+                const positivesRegex = /(?<=positives\\[)[^\\]]+?(?=\\])/g
+                const negativesRegex = /(?<=negatives\\[)[^\\]]+?(?=\\])/g
+                let pos = info.match(posRegex) ? parseInt(info.match(posRegex)[0]) : 0
+                let line = info.match(lineRegex) ? parseInt(info.match(lineRegex)[0]) : 0
+                let col = info.match(colRegex) ? parseInt(info.match(colRegex)[0]) : 0
+                // log(info.match(positivesRegex))
+                let positives = info.match(positivesRegex) ? info.match(positivesRegex)[0].replace("EOI", "END OF INPUT").split(",").join(" ||") : ""
+                let negatives = info.match(negativesRegex) ? info.match(negativesRegex)[0].split(",").join(" or") : ""
+                // log(pos, line, col, positives, negatives)
+                log(\`%cError at line \${line}\`, "background: #3b82f6; color:white; font-weight: bold")
+                
+                let errline = window.code.split("\\n")[line-1];
+                let styleErrLine = errline.slice(0, col-1) + "%c %c" + errline.slice(col-1);
+                log(styleErrLine, "font-weight: bold; background: #f472b6; color:white", "");
+                
+                let positiveResult = positives.length > 0?
+                "expecting "+positives:""
+                log(
+                  \`\${"_".repeat(col-1 >=0?col-1:0)}%c^^^ \${positiveResult}\${negatives.length > 0?"unexpected"+negatives:""}\`,
+                    "font-weight: bold; background: #f472b6; color:white");`)
+              // )
             }
 
             // log()
@@ -365,9 +387,27 @@ window.run = async (code) =>{
   // a working JS mix
   
   if (!name.includes("Safari")) {
-    await fetch(source+`regex.js`).then(res=>res.text()).then( text => // ${window.version ? ".min": ""}
-      code = eval(text)
-    )
+    let toeval = 
+`const regexp = /(?<=##)[^#]*(?=#)/g;
+let match;
+let toreplace = [];
+while ((match = regexp.exec(code)) !== null) {
+toreplace.push(match[0])
+};
+
+toreplace.map((str)=>{
+    let result = str.includes('\\n') || str.includes(';') ?
+    Function(\`'use strict'; return ()=>{\${str}}\`)()() : 
+    Function(\`'use strict'; return ()=>(\${str})\`)()();
+
+    if (typeof result !== "undefined") {
+        code = code.replace(\`##\${str}#\`, result)
+    } else {
+        code = code.replace(\`##\${str}#\`, "")
+    };
+});
+code`;
+    code = eval(toeval)
   } else {
     if (code.includes("#")) {
       alert("Safari does not support mix JS as it fails to support basic Regex.")
