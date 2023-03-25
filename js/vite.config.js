09d8ea3eb43258a316e8ed6037dc7ee80db5bdbf
chaosprint <chaosprint@gmail.com>
2023-03-25 13:49:32 +0100

    no need for vite plugin

--- js/vite.config.js
+++ js/vite.config.js
@@ -1,5 +1,16 @@
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
+import { defineConfig } from "vite";
 
-export default {
-  plugins: [crossOriginIsolation()]
-}
+export default defineConfig({
+  plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
+});
