0b7690b9aee73ba535ca9a4ecbcc2252be39c6a9
itsjunetime <junewelker@gmail.com>
2024-04-19 13:54:36 -0600

    - Apply clippy fixes to oscillators - Extract shared logic for oscillators to fn - Use zipped iterators in oscillators where possible instead of direct indexing - Use destructing instead of indexing for slices in oscillators where possible

--- rs/synth/src/node/oscillator/mod.rs
+++ rs/synth/src/node/oscillator/mod.rs
@@ -6,3 +6,36 @@ pub use tri_osc::TriOsc;
 mod tri_osc;
 pub use squ_osc::SquOsc;
 mod squ_osc;
+
+use crate::{Buffer, Input};
+use hashbrown::HashMap;
+
+fn process_oscillation<const N: usize>(
+    inputs: &mut HashMap<usize, Input<N>>,
+    input_order: &mut [usize],
+    output: &mut [Buffer<N>],
+    freq: f32,
+    inc: &mut f32,
+    mut osc: impl FnMut(&mut f32, f32)
+) {
+    match inputs.len() {
+        0 => for out in &mut *output[0] {
+            osc(out, freq);
+        },
+        1 => {
+            let mod_input = match input_order {
+                [] => &mut *inputs.values_mut().next().unwrap(),
+                [ref first_input, ..] => &inputs[first_input],
+            };
+
+            for (out, mod_buf) in output[0].iter_mut().zip(mod_input.buffers()[0].iter()) {
+                if *mod_buf != 0. {
+                    *inc = *mod_buf;
+                };
+
+                osc(out, *inc);
+            }
+        }
+        _ => {}
+    }
+}
--- rs/synth/src/node/oscillator/saw_osc.rs
+++ rs/synth/src/node/oscillator/saw_osc.rs
@@ -1,5 +1,7 @@
 use crate::{impl_to_boxed_nodedata, BoxedNodeSend, Buffer, Input, Message, Node, NodeData};
 use hashbrown::HashMap;
+
+use super::process_oscillation;
 #[derive(Debug, Clone)]
 pub struct SawOsc {
     pub freq: f32,
@@ -39,42 +41,18 @@ impl SawOsc {
 
 impl<const N: usize> Node<N> for SawOsc {
     fn process(&mut self, inputs: &mut HashMap<usize, Input<N>>, output: &mut [Buffer<N>]) {
-        match inputs.len() {
-            0 => {
-                for i in 0..N {
-                    output[0][i] = self.phase * 2. - 1.;
-                    self.phase += self.freq / self.sr as f32;
-                    if self.phase > 1. {
-                        self.phase -= 1.
-                    }
-                }
+        process_oscillation(inputs, &mut self.input_order, output, self.freq, &mut self.inc, |out, freq| {
+            *out = self.phase * 2. - 1.;
+            self.phase += freq / self.sr as f32;
+            if self.phase > 1. {
+                self.phase -= 1.
             }
-            1 => {
-                let mod_input = match self.input_order.len() {
-                    0 => &mut *inputs.values_mut().next().unwrap(),
-                    _ => &inputs[&self.input_order[0]],
-                };
-                let mod_buf = mod_input.buffers();
-                for i in 0..N {
-                    output[0][i] = self.phase * 2. - 1.;
-                    if mod_buf[0][i] != 0. {
-                        self.inc = mod_buf[0][i]
-                    };
-                    self.phase += self.inc / self.sr as f32;
-                    if self.phase > 1. {
-                        self.phase -= 1.
-                    }
-                }
-            }
-            _ => return (),
-        }
+        });
     }
+
     fn send_msg(&mut self, info: Message) {
         match info {
-            Message::SetToNumber(pos, value) => match pos {
-                0 => self.freq = value,
-                _ => {}
-            },
+            Message::SetToNumber(0, value) => self.freq = value,
             Message::Index(i) => self.input_order.push(i),
             Message::IndexOrder(pos, index) => self.input_order.insert(pos, index),
             Message::ResetOrder => {
