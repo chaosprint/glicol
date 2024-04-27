29b6ce5567f56bd3b1c20758c40440c19c5da747
itsjunetime <junewelker@gmail.com>
2024-04-27 17:44:16 -0600

    Allow comments within chains in grammar

--- rs/parser/tests/sin.rs
+++ rs/parser/tests/sin.rs
@@ -5,3 +5,20 @@ fn minimal() {
     let res = get_ast("o: sin 440");
     assert!(res.is_ok());
 }
+
+#[test]
+fn comment_within_chain() {
+    let res = get_ast(
+        "o: sin 440
+    // >> mul 0.5
+    >> mul 0.6"
+    );
+    assert!(res.is_ok());
+
+    let res = get_ast(
+        "o: sin 440
+// ooooh this is nonsense
+>> add 6.00"
+    );
+    assert!(res.is_ok());
+}
